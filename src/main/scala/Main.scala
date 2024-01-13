737d5c50c79619cd61573075696c1a523fe69d0e
Amirabbas Riazi <Amirabbas.Riazi@snapptrip.com>
2024-01-13 12:17:07 +0330

    feat: add date range and custom interval from user to order count calculation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,14 @@
+import repository.DatabaseProvider
+import repository.order.OrderRepositoryImpl
+import service.OrderService
+
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
+import java.time.Instant
 import java.util.concurrent.Executors
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext}
+import scala.util.Try
 
 object Main {
 
@@ -10,9 +17,41 @@ object Main {
   final val defaultDate = Timestamp.from(dateFormat.parse("2022-05-10 14:20:00").toInstant)
   private final val defaultIntervals = Seq(1 -> 3, 4 -> 6, 7 -> 12)
 
+  val parseTimestamp: String => Timestamp = strDate => Timestamp.from(dateFormat.parse(strDate).toInstant)
+
+
   def main(args: Array[String]): Unit = {
 
+    lazy val orderService = new OrderService(new OrderRepositoryImpl(DatabaseProvider.db))
+
+    val dateFrom = Try(
+      parseTimestamp(args(0)))
+      .toOption
+      .getOrElse(defaultDate)
 
 
+    val dateTo = Try(
+      parseTimestamp(args(1)))
+      .toOption
+      .getOrElse(Timestamp.from(Instant.now()))
+
+    val intervals = args.
+      drop(2)
+      .map(_.split("-").map(_.toInt))
+      .map(interval => interval(0) -> interval(1))
+      .toSeq match {
+      case Seq() => defaultIntervals
+      case interval => interval
+    }
+
+    println("Orders grouped by product age:")
+
+    Await.result(
+      orderService.getOrderCount(dateFrom, dateTo, intervals),
+      5.seconds
+    ).foreach {
+      case (months, ordersInInterval) =>
+        println(s"$months months: ${ordersInInterval} orders")
+    }
   }
 }
