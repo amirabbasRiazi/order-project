ac2ccebf00decf9835997f165179db4cc5142b03
Amirabbas Riazi <Amirabbas.Riazi@snapptrip.com>
2024-01-13 13:06:02 +0330

    Add unit test or orderService groupOrdersByProductAge and testDetermineIntervalLabel method

---
+++ src/test/scala/service/OrderServiceTest.scala
@@ -0,0 +1,19 @@
+package service
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class OrderServiceTest extends AnyFunSuiteLike {
+
+  test("testDetermineIntervalLabel") {
+
+  }
+
+  test("testGroupOrdersByProductAge") {
+
+  }
+
+  test("testCalculateProductAgeMonths") {
+
+  }
+
+}
